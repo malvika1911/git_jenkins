196e352aa5657430325af336083e2eaafd2807e3
malvika1911 <121507694+malvika1911@users.noreply.github.com>
2025-02-11 16:18:51 +0530

    Update pull.py

--- pull.py
+++ pull.py
@@ -4,7 +4,7 @@ pipeline {
         stage('Test') {
             steps {
                 bat 'python --version'
-                bat 'ls -l'  // Debugging step to check if pyg1.py exists
+                bat 'dir'  // Debugging step to check if pyg1.py exists
                 bat 'python pyg1.py'  // Ensure this script exists in the workspace
             }
         }
