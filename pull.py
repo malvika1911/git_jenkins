12a94e8a9aac5edc0242a57dcd08bd06633ef829
malvika1911 <121507694+malvika1911@users.noreply.github.com>
2025-02-11 16:10:18 +0530

    Update pull.py

--- pull.py
+++ pull.py
@@ -1,10 +1,9 @@
 pipeline {
-    agent {
+    agent any {
     stages {
         stage('Test') {
             steps {
                 bat 'python --version'
-                bat 'ls -l'  // Debugging step to check if pyg1.py exists
                 bat 'python pyg1.py'  // Ensure this script exists in the workspace
             }
         }
